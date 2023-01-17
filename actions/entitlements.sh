111bae652baeab2ee5bfcc0ccb1c340c8a1d4351
nirappdome <119943675+nirappdome@users.noreply.github.com>
2023-01-17 10:21:44 +0200

    Create entitlements.sh

---
+++ actions/entitlements.sh
@@ -0,0 +1,16 @@
+VAR=$1
+echo $VAR
+counter=1
+for i in $VAR
+  do
+    if [[ $i == htt* ]]; then
+      echo "Downloading entitlement file"
+      wget $i -O "files/entitlements/${counter}.plist"
+    elif [[ $VAR != '!' ]]; then
+      echo "Copy entitlement file"
+      cp $i "files/entitlements/${counter}.plist"
+    else
+      echo "entitlement file not provided"
+    fi
+    ((counter++))
+  done
